63a3087a2891e3f081e5dc0fd0fe2b5790496b76
petbod-lm <peter-alexander.bodley@lm.se>
2022-10-05 13:42:53 +0200

    first commit

---
+++ src/main/java/com/example/pete/PeteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pete/PeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
